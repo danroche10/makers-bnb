ff53a2b399572e5d6466cbbcf17e72d8872132eb
ian k <kickthechair@gmail.com>
2021-08-24 15:49:27 +0100

    added feature test to add a property and display it. Added feature test to only show non booked spaces

--- spec/feature/view_spaces_spec.rb
+++ spec/feature/view_spaces_spec.rb
@@ -7,3 +7,12 @@ feature 'list all spaces' do
     expect(page).to have_content('100000')
   end
 end
+
+feature 'list all spaces' do
+  scenario 'as a logged in user I can see all available spaces' do
+    add_test_spaces
+    Space.book(1)
+    visit('/makersbnb/spaces')
+    expect(page).not_to have_content('space1')
+  end
+end
