41ffc8d67688112fa2f1e6167231fcda15413afc
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-08-24 11:32:36 +0100

    Progress on unit testing space class

--- lib/space.rb
+++ lib/space.rb
@@ -2,9 +2,10 @@ require 'pg'
 
 class Space
 
-  attr_reader :name, :description, :price, :booked
+  attr_reader :id, :name, :description, :price, :booked
 
-  def initialize(name, description, price, booked)
+  def initialize(id, name, description, price, booked = false)
+    @id = id
     @name = name
     @description = description
     @price = price
@@ -12,15 +13,29 @@ class Space
   end
 
   def self.all
-
-    if ENV['ENVIRONMENT'] = 'test'
-      PG.connect(dbname: makers_bnb_test)
-    else
-      PG.connect(dbname: makers_bnb)
-
-    #connect to DB
-    #iterate through the test database rows through .map
-    #create new space objects from each row
+    connect_db
+    @connection.exec('SELECT * FROM spaces').map do |space| 
+      Space.new(space['id'], space['name'], space['description'], space['price'], space['booked'])
+    end
   end
+
+  def self.create(name, description, price)
+    connect_db
+    result = @connection.exec_params('INSERT INTO spaces (name, description, price)' \
+    'VALUES ($1, $2, $3) RETURNING id ;', [name, description, price])
+    Space.new(result['id'], name, description, price)
+    end
+  end
+
+
+
+  def self.connect_db
+    if ENV['ENVIRONMENT'] = 'test'
+      @connection = PG.connect(dbname: 'makers_bnb_test')
+    else
+      @connection = PG.connect(dbname: 'makers_bnb')
+    end
+  end
+
 
 end
--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -2,17 +2,33 @@ require 'space'
 
 describe Space do
 
-  let(:space) { described_class.new("space1", "semi comfortable", 10000, false)} 
+  let(:space) { described_class.new(1, "space1", "semi comfortable", 10000, false) }
+  let(:spaces) { Space.all }
 
   describe '#initialize' do
     it 'initializes with a name, description, price and booking status' do
-      expect(space).to have_attributes(name: "space1", description: "semi comfortable", price: 10000, booked: false)
+      expect(space).to have_attributes(id: 1, name: "space1", description: "semi comfortable", price: 10000, booked: false)
     end
   end
 
-  describe'#all' do
-    it 'list all spaces'
-      expect(Space.all).to include(Space)
+  describe '#self.all' do
+    it 'list all spaces' do
+      add_test_spaces
+      expect(spaces.to include(Space)
+      expect([spaces.first.id, spaces.last.id]).to eq([1, 3])
+      expect([spaces.first.name, spaces.last.name]).to eq(['space1', 'space3'])
+      expect([spaces.first.description, spaces.last.description]).to eq(['semi comfortable', 'uncomfortable'])
+      expect([spaces.first.price, spaces.last.price]).to eq([10000, 100000])
+    end
   end
 
+  describe '#self.create' do
+    it 'add space to database and create new space instance' do
+      new_space = Space.create('space1', 'semi comfortable', 10000)
+      expect(new_space).to eq(Space)
+      expect(new_space.name).to eq('space1')
+      expect(new_space.description).to eq('semi comfortable')
+      expect(new_space.price).to eq(10000)
+    end
+  end
 end
