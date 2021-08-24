1e0f62040be601ae4ad3bec023487d3d2cae65a5
moby-codes <mubashar.jaffri@gmail.com>
2021-08-24 14:40:46 +0100

    refactored tests - added connectdb method - added initialize test

--- lib/user.rb
+++ lib/user.rb
@@ -11,40 +11,36 @@ class User
   end
 
   def self.create(email:, password:)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'makers_bnb_test')
-    else
-      con = PG.connect(dbname: 'makers_bnb')
-    end
-    result = con.exec(
+    connect_db
+    result = @con.exec(
       "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email, password;", [email, password])
     User.new(id: result[0]['id'], email: result[0]['email'], password: result[0]['password'])
   end
 
   def self.find(id)
     return nil unless id
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'makers_bnb_test')
-    else
-      con = PG.connect(dbname: 'makers_bnb')
-    end
-    result = con.exec(
+    connect_db
+    result = @con.exec(
       "SELECT * FROM users WHERE id = $1", [id])
       User.new(id: result[0]['id'], email: result[0]['email'], password: result[0]['password']) 
   end
 
   def self.authenticate(email:, password:)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect(dbname: 'makers_bnb_test')
-    else
-      con = PG.connect(dbname: 'makers_bnb')
-    end
-    result = con.exec("SELECT * FROM users WHERE email = $1", [email])
+    connect_db
+    result = @con.exec("SELECT * FROM users WHERE email = $1", [email])
     return unless result.any?
 
     return unless (result[0]['password']) == password
 
     User.new(id: result[0]['id'], email: result[0]['email'], password: result[0]['password']) 
-
   end
+
+  def self.connect_db
+    if ENV['ENVIRONMENT'] = 'test'
+      @con = PG.connect(dbname: 'makers_bnb_test')
+    else
+      @con = PG.connect(dbname: 'makers_bnb')
+    end
+  end
+
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -2,10 +2,18 @@ require 'user'
 
 describe User do
 
+  let(:user) { described_class.create(email: 'test@test.com', password: 'test') }
+
+
+  describe '#initialize' do
+    it 'initializes with an id, email, password' do
+      expect(user).to have_attributes(id: '1', email: 'test@test.com', password: 'test')
+    end
+  end
+
   describe '.create' do
     it 'creates new user' do
-      user = described_class.create(email: 'test@test.com', password: 'test')
-      expect(user.id).not_to eq nil
+      expect(user.id).to eq '1'
       expect(user.email).to eq 'test@test.com'
       expect(user.password).to eq 'test'
     end
@@ -17,7 +25,6 @@ describe User do
     end
     
     it 'finds user by id' do
-      user = described_class.create(email: 'test@test.com', password: 'test')
       result = described_class.find(user.id)
       expect(result.email).to eq user.email
       expect(result.id).to eq user.id
@@ -34,14 +41,10 @@ describe User do
     end
 
     it 'returns nil given an incorrect email address' do
-      User.create(email: 'test@test.com', password: 'password123')
-
       expect(User.authenticate(email: 'nottherightemail@me.com', password: 'password123')).to be_nil
     end
 
     it 'returns nil given an incorrect password' do
-      User.create(email: 'test@test.com', password: 'password123')
-      
       expect(User.authenticate(email: 'test@test.com', password: 'wrongpassword')).to be_nil
     end
   end
