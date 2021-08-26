eb8326b6b940b9ad163b4597d869a2953d2ecd1d
danroche10 <danielroche10@gmail.com>
2021-08-26 12:14:36 +0100

    updated headers, where constrain, flash[:notice]

--- spec/feature/index_spec.rb
+++ spec/feature/index_spec.rb
@@ -32,7 +32,17 @@ feature "user can sign up for makers bnb" do
     expect(page).to have_content "Book A Space"
     expect(page).to have_button "Log out"
   end
+
+  scenario "user see error when they enter an incorrect password" do
+    visit '/makersbnb'
+    fill_in('email', with: 'test@test.com')
+    fill_in('password', with: 'test')
+    click_button('Sign up')
+    fill_in('password_confirmation', with: 'wrongpassword')
+    expect(page).to have_content "You have entered incorrect details"
+  end
 end
+
 feature "Once logged in, users can log out" do
   scenario "by clicking the log out button" do
     new_user_login
